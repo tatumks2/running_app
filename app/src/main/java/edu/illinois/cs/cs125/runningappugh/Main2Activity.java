f926fc1859ff3a2497e0e923de7bedb03053f769
Anand Haran <haran3@illinois.edu>
2018-05-01 07:07:25 -0500

    pedometer sensor

--- app/src/main/java/edu/illinois/cs/cs125/runningappugh/Main2Activity.java
+++ app/src/main/java/edu/illinois/cs/cs125/runningappugh/Main2Activity.java
@@ -13,7 +13,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
